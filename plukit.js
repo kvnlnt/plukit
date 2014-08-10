71ca9022e34f909ef02fcb7a2dbaf78fef72d28a
kvnlnt <kvnlnt@yahoo.com>
2014-08-10 14:26:43 -0700

    add spites

---
+++ plukit.js
@@ -0,0 +1,20 @@
+var Plukit = {
+    guitar: {
+        acoustic: {
+            nylon: {
+                mp3: 'gtr_aco_nylon.mp3'
+            },
+            steel: {
+                mp3: 'gtr_aco_steel.mp3'
+            }
+        },
+        electric: {
+            clean: {
+                mp3: 'gtr_elec_clean.mp3'
+            },
+            distortion: {
+                mp3: 'gtr_elec_dist'
+            }
+        }
+    }
+};
